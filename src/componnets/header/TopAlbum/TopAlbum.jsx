93110e7c68a7ae1c648b434740a0c91b65d08a65
Vishal Bansal <saibansal87@gmail.com>
2025-09-04 07:42:51 +0000

    refactor: remove AlbumCard component and integrate NewAlbum and TopAlbum components

---
+++ src/componnets/header/TopAlbum/TopAlbum.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useRef, useEffect } from "react";
+import Slider from "react-slick";
+import axios from "axios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./album.scss";
+import {
+  Card,
+  CardBody, 
+  CardTitle,
+  Badge,
+} from "reactstrap";
+
+function TopAlbum() {
+  const [nav1, setNav1] = useState(null);
+  const sliderRef1 = useRef(null);
+
+  const [albums, setAlbums] = useState([]);
+  const [showAll, setShowAll] = useState(false); // 👈 toggle state
+
+  useEffect(() => {
+    setNav1(sliderRef1.current);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://qtify-backend-labs.crio.do/albums/top")
+      .then((res) => {
+        setAlbums(res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching albums:", err);
+      });
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    centerPadding: "60px",
+    lazyLoad: true,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: { slidesToShow: 2, slidesToScroll: 2, initialSlide: 2 },
+      },
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 1, slidesToScroll: 1 },
+      },
+    ],
+  };
+
+  return (
+    <div className="top-albums">
+      <h2 className="sectionHeading">
+        Top Albums{" "}
+        <a 
+          onClick={(e) => {
+            e.preventDefault();
+            setShowAll(!showAll);
+          }}
+        >
+          {showAll ? "Collapse" : "Show All"}
+        </a>
+      </h2>
+
+      {showAll ? (
+        <div class="row row-cols-1 row-cols-md-6 g-4 slider-container">
+          {albums.map((album) => (
+            <div class="col">
+              <Card key={album.id}>
+                <div className="infoContainer">
+                  <img alt={album.title} src={album.image} />
+                  <CardBody>
+                    <Badge color="dark" className="followBadge" pill>
+                      {album.follows} Follows
+                    </Badge>
+                  </CardBody>
+                </div>
+                <CardTitle tag="h5">{album.title}</CardTitle>
+              </Card>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="slider-container">
+          <Slider {...settings} asNavFor={nav1} ref={sliderRef1}>
+            {albums.map((album) => (
+              <div key={album.id} className="cardParent">
+                <Card>
+                  <div className="infoContainer">
+                    <img alt={album.title} src={album.image} />
+                    <CardBody>
+                      <Badge color="dark" className="followBadge" pill>
+                        {album.follows} Follows
+                      </Badge>
+                    </CardBody>
+                  </div>
+                  <CardTitle tag="h5">{album.title}</CardTitle>
+                </Card>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TopAlbum;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "../componnets/header/Header";
 import { Container } from "reactstrap";
-import AlbumCard from "../componnets/header/albumcard/AlbumCard";
+
 import "./home.scss";
+import NewAlbum from "../componnets/header/NewAlbum/NewAlbum";
+import TopAlbum from "../componnets/header/TopAlbum/TopAlbum";
 
 function Home() {
   return (
@@ -11,14 +13,8 @@ function Home() {
 
       <main className="main" style={{ backgroundColor: "#121212" }}>
         <Container>
-          {/* top albums section finish */}
-          <div className="top-albums">
-            <h2 className="sectionHeading">Top Albums <a href="#"> Show All </a> </h2>
-
-            <AlbumCard />
-          </div>
-          {/* top albums section starts */}
-
+          <TopAlbum />
+          <NewAlbum />
         </Container>
       </main>
     </>
